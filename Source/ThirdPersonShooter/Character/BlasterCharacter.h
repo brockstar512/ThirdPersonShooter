1b82fb6ddda6a00f85398f5f5f8c646c6a35623b
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2023-06-20 13:34:14 -0500

    saving changes

--- Source/ThirdPersonShooter/Character/BlasterCharacter.h
+++ Source/ThirdPersonShooter/Character/BlasterCharacter.h
@@ -12,18 +12,28 @@ class THIRDPERSONSHOOTER_API ABlasterCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ABlasterCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void Turn(float Value);
+	void LookUp(float Value);
 
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = CameraAnywhere)
+	class USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, Category = CameraAnywhere)
+	class UCameraComponent* FollowCamera;
+	
+
+//getters and setters
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
