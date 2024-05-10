b9c4c23b2d5974c70eb48387326387521a6d5654
brockstar512 <MARSHALLBROCK@GMAIL.COM>
2024-05-10 14:16:19 -0500

    added pistol

--- Source/ThirdPersonShooter/BlasterComponents/CombatComponent.cpp
+++ Source/ThirdPersonShooter/BlasterComponents/CombatComponent.cpp
@@ -111,6 +111,8 @@ void UCombatComponent::InitializeCarriedAmmo()
 {
 	CarriedAmmoMap.Emplace(EWeaponType::EWT_AssaultRifle,StartingARAmmo);
 	CarriedAmmoMap.Emplace(EWeaponType::EWT_RocketLauncher, StartingRocketAmmo);
+	CarriedAmmoMap.Emplace(EWeaponType::EWT_Pistol, StartingPistolAmmo);
+
 
 }
 
--- Source/ThirdPersonShooter/BlasterComponents/CombatComponent.h
+++ Source/ThirdPersonShooter/BlasterComponents/CombatComponent.h
@@ -112,6 +112,8 @@ private:
 	int32 StartingARAmmo = 30;
 	UPROPERTY(EditAnywhere)
 	int32 StartingRocketAmmo = 0;
+	UPROPERTY(EditAnywhere)
+	int32 StartingPistolAmmo = 0;
 
 	UPROPERTY(ReplicatedUsing = OnRep_CombatState)
 	ECombatState CombatState = ECombatState::ECS_Unoccupied;
